fc44dbe602d3641ec37d1ac1e2660f930e3db009
rmaakli <rmaakli@ST211>
2019-11-05 17:21:47 +0100

    Maj

--- Model1.Designer.cs
+++ Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'C:\Users\eunlu\Source\Repos\PPE3_GSB_BalemrogV2\Model1.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\Users\rmaakli\Source\Repos\PPE3_GSB_BalemrogV2\Model1.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
